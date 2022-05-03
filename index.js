b7434d0913a272a323d394e833b6b7fed8edc22a
Warren <hello@warren.codes>
2022-05-03 12:07:47 -0600

    Add read/write cookie example.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
+
+// Set middleware for easy cookie parsing.
+app.use(cookieParser());
 
 // Set middleware for static assets.
 app.use(express.static('public')); // Any route that matches a file in /public will be served directly.
@@ -13,6 +16,10 @@ app.set('view engine', 'ejs');
 
 // Home page.
 app.get('/', (req, res) => {
+    res.cookie('testCookie', '12345'); // Sets a cookie (key, value).
+    if (req.cookies.testCookie) { // Retrieves a cookie (req.cookes.cookieKey)
+        console.log('Cookie found! testCookie:', req.cookies.testCookie);
+    }
     res.render('index');
 });
 
