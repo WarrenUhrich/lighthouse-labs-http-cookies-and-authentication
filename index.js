45ba202d9b55b9a1e5602ea48858e39daf44aae3
Warren <hello@warren.codes>
2022-05-03 11:50:41 -0600

    Add basic homepage to index.js.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
 
+// Set middleware for static assets.
+app.use(express.static('public')); // Any route that matches a file in /public will be served directly.
 
+// Let express know we are using a template engine.
+app.set('view engine', 'ejs');
+
+// Home page.
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log('Express server is running on port:', port);
+});
