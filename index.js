e2689a3603c77017d4ef0a1abcb23809b3d0527c
Warren <hello@warren.codes>
2022-06-28 12:08:47 -0600

    Add sign-in page and static assets.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const PORT = 3000;
 const app = express();
 
+app.use(express.static('public')); // Choose a directory to serve files from... (images, CSS, JS, videos.)
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -16,9 +17,17 @@ app.listen(PORT, () => {
 
 // Cookie read and write test.
 app.get('/test', (req, res) => {
-    // Writing a cookie (key, value).
-    res.cookie('test cookie', 'This is a test!');
     // Reading a cookie from the req.cookies object.
     console.log('Cookie Received on Back-end:', req.cookies['test cookie']);
+    // Writing a cookie (key, value).
+    res.cookie('test cookie', 'This is a test!');
+    // Send HTML to browser.
     res.end('<html><head><title>test</title></head><body>test</body></html>');
+});
+
+app.get('/', (req, res) => {
+    const templateVars = {
+        pageName: 'Sign In',
+    };
+    res.render('index', templateVars);
 });
