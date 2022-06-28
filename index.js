7261be994c8e809f31b34b39bedba4a97208592b
Warren <hello@warren.codes>
2022-06-28 11:27:24 -0600

    Add basic Express app.

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const PORT = 3000;
-
 const app = express();
 
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
+
+app.listen(PORT, () => {
+    console.log('Express server running on port:', PORT);
+});
+
+app.get('/', (req, res) => {
+    res.end('Hello, World!');
+});
